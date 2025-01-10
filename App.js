31a8bdc723085847f9b58e28b338a2cdbeed41ef
anisimaa <isla.anisimaa00@gmail.com>
2025-01-10 17:08:07 +0200

    buttons, text and styling added

--- App.js
+++ App.js
@@ -1,21 +1,91 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import './styles.css';
+import React from 'react';
+import { View, TouchableOpacity, ScrollView, Alert, ImageBackground, Text, Dimensions } from 'react-native';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import styles from './styles';
 
 export default function App() {
+  {/*Button press alerts*/}
+  const handleButtonPress1 = () => {
+    Alert.alert('Topbutton Pressed!', 'You pressed the top button!');
+  };
+
+  const handleButtonPress2 = () => {
+    Alert.alert('Cardbutton Pressed!', 'You pressed the card button!');
+  };
+
+  const handleButtonPress3 = () => {
+    Alert.alert('Card Pressed!', 'You pressed the card!');
+  };
   return (
-    <View style={styles.container}>
-      <Text>HELLO</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView style={styles.scrollContainer}>
+      <ImageBackground
+        source={require('./assets/workoutback2.jpg')}
+        style={styles.imageBackground}
+        resizeMode="repeat"
+      >
+
+      {/* Header Text */}
+      <Text style={styles.headerText}>Workout diary</Text>
+       
+      <View style={styles.buttonContainer}>
+      
+      {/*Top button 1 */}
+      <TouchableOpacity style={styles.button} onPress={handleButtonPress1}>
+        {/* FontAwesome Icon */}
+       <FontAwesome marginRight= '10' marginTop= '4' name="plus-circle" size={15} color="#FFFFFF" />
+            <Text style={styles.buttonText}>New sport</Text>
+          </TouchableOpacity>
+
+        {/*Top button 2 */}
+      <TouchableOpacity style={styles.button} onPress={handleButtonPress1}>
+            {/* FontAwesome Icon */}
+       <FontAwesome marginRight= '10' marginTop= '4' name="list" size={15} color="#FFFFFF" />  
+            <Text style={styles.buttonText}>All exercises</Text>
+          </TouchableOpacity>
+          </View>
+
+        {/* Card Container1 */}
+        <TouchableOpacity style={styles.card} onPress={handleButtonPress3}>
+          <Text style={styles.cardTitle}>Running</Text>
+          <Text style={styles.cardText}>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</Text>  
+        {/* Cardbutton */}
+      <TouchableOpacity style={styles.cardButton} onPress={handleButtonPress2}>
+            <Text style={styles.buttonText}>Add exercise</Text>
+          </TouchableOpacity>
+          </TouchableOpacity>
+
+          {/* Card Container2 */}
+        <TouchableOpacity style={styles.card} onPress={handleButtonPress3}>
+          <Text style={styles.cardTitle}>Walking</Text>
+          <Text style={styles.cardText}>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</Text>       
+        {/* Cardbutton */}
+      <TouchableOpacity style={styles.cardButton} onPress={handleButtonPress2}>
+            <Text style={styles.buttonText}>Add exercise</Text>
+          </TouchableOpacity>
+          </TouchableOpacity>
+
+          {/* Card Container3 */}
+        <TouchableOpacity style={styles.card} onPress={handleButtonPress3}>
+          <Text style={styles.cardTitle}>Swimming</Text>
+          <Text style={styles.cardText}>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</Text>      
+        {/* Cardbutton */}
+      <TouchableOpacity style={styles.cardButton} onPress={handleButtonPress2}>
+            <Text style={styles.buttonText}>Add exercise</Text>
+          </TouchableOpacity>
+        </TouchableOpacity>
+
+        {/* Card Container4 */}
+        <TouchableOpacity style={styles.card} onPress={handleButtonPress3}>
+          <Text style={styles.cardTitle}>Walking</Text>
+          <Text style={styles.cardText}>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</Text>
+        {/* Cardbutton */}
+      <TouchableOpacity style={styles.cardButton} onPress={handleButtonPress2}>
+            <Text style={styles.buttonText}>Add exercise</Text>
+          </TouchableOpacity>
+        </TouchableOpacity>
+
+      </ImageBackground>
+    </ScrollView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ styles.js
@@ -0,0 +1,89 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  scrollContainer: {
+    flexgrow: 1, 
+    backgroundColor: '#FFFFFF', 
+  },
+
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+
+  imageBackground: {
+    flex: 1,
+    width: '100%',
+    justifyContent: 'center', 
+    alignItems: 'center', 
+    justifyContent: 'flex-start', 
+    alignItems: 'center', 
+    backgroundRepeat: 'repeat', 
+    paddingBottom: 50,
+  },
+  card: {
+    backgroundColor: 'rgba(255, 255, 255, 0.87)', 
+    padding: 20,
+    borderRadius: 10,
+    width: '85%',              
+    alignItems: 'center',      
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 5 },
+    shadowOpacity: 0.3,
+    shadowRadius: 10,
+    marginBottom: 20,
+  },
+  cardTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    color: '#333',
+  },
+  cardText: {
+    fontSize: 16,
+    color: '#333',
+    textAlign: 'center',
+  },
+
+  headerText: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    color: '#333', 
+    marginTop: 90,
+  },
+
+  buttonContainer: {
+    flexDirection: 'row', 
+    justifyContent: 'space-around', 
+    width: '100%',
+    paddingTop: 30,
+    paddingHorizontal: 20,
+    top: 0,
+    zIndex: 10, 
+  },
+  button: {
+    flexDirection: 'row',
+    backgroundColor: '#000000', 
+    paddingVertical: 10,
+    paddingHorizontal: 19,
+    borderRadius: 5,
+    marginBottom: 30,
+  },
+
+  cardButton: {
+    backgroundColor: '#000000', 
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+    marginTop: 20,
+    marginBottom: 10,
+  },
+  buttonText: {
+    color: '#fff', // White text
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
+
+export default styles;
